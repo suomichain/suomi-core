5dca48f957eb810c7d7fa705975dd6815b23b58e
Leifu Chen <leifu.chen@gmail.com>
2018-04-22 02:22:58 +0300

    Updated the consensus algorithm

---
+++ consensus/fastpow/cli/setup.py
@@ -0,0 +1,41 @@
+# Copyright 2017 Suomi Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ------------------------------------------------------------------------------
+
+from __future__ import print_function
+
+import subprocess
+
+from setuptools import setup, find_packages
+
+setup(
+    name='suomi-poet-cli',
+    version=subprocess.check_output(
+        ['../../../bin/get_version']).decode('utf-8').strip(),
+    description='Suomi PoET CLI',
+    author='Suomi',
+    url='https://github.com/suomi/suomi-core',
+    packages=find_packages(),
+    install_requires=[
+        'colorlog',
+        'protobuf',
+        'suomi-poet-common',
+        'suomi-poet-core',
+        'suomi-signing',
+        'suomi-validator',
+        'toml',
+    ],
+    entry_points={
+        'console_scripts': ['poet = suomi_poet_cli.main:main_wrapper']
+    })
